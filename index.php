9dd36f25f3df3dc2bb5586e3420e6d962ce3b68f
Nickie <nickie.roudez@gmail.com>
2017-05-03 16:43:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/css/style.css">
+    <title>CV Nickie Roudez</title>
+</head>
+<body>
+    <h1>Mon petit CV</h1>
+    <h2>Bienvenue</h2>
+</body>
+</html>
